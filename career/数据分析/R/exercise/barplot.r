27fe90203dee38444b6a350af45a077893c5182e
Ivan <wcy5211678@163.com>
2023-12-09 11:16:02 +0800

    chore: update

--- career/数据分析/R/exercise/barplot.r
+++ career/数据分析/R/exercise/barplot.r
@@ -3,5 +3,18 @@ f= c(10,19,46,18,3)
 font_files()
 showtext_auto(enable = TRUE)
 # font_add('Songti')
+
 # quartz(family = 'ArialUnicodeMS')
-barplot(f, col=2:6, names.arg = c("高中", "专科", "本科", "硕士研究生", "博士研究生"))
+
+# 自定义 柱状体
+# barplot(f, col=2:6, names.arg = c("高中", "专科", "本科", "硕士研究生", "博士研究生"))
+# 设置坐标轴的名称和颜色
+# title(xlab = "学历", ylab = "人数")
+
+
+# ![](https://raw.githubusercontent.com/lih627/MyImages/master/images/%E5%AD%A6%E5%8E%86.png)
+
+a = read.csv("~/workspace/blog/career/数据分析/R/exercise/表格.csv")
+attach(a)
+
+barplot(table(a[,1]), col = 2:6)
--- career/数据分析/R/exercise/table.r
+++ career/数据分析/R/exercise/table.r
@@ -1,4 +1,17 @@
 a = read.csv("~/workspace/blog/career/数据分析/R/exercise/表格.csv")
 attach(a)
+
+# table
+table(a[,1])
+table(a[,2])
 table(a)
 
+# ftable
+ftable(a[,1],row.vars=1)
+ftable(a[,2],row.vars=1)
+ftable(a,row.vars=1)
+
+# xtabs
+xtabs(~文化程度, a)
+xtabs(~性别, a)
+xtabs(~., a)
