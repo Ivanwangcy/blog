007e56b6fd4d6516d3612e4d47775be80d7186d5
Ivan <wcy5211678@163.com>
2024-11-06 15:18:38 +0800

    chore: update

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -2,9 +2,12 @@
 
 int main() {
   int a = 10, b = 40;
+  int c, d;
+
   printf("%d + %d = %d\n",a , b, a + b);
   printf("hello world\nHello Geek!");
+  scanf("%d %d", &c, &d);
+  printf("%d + %d = %d\n",c , d, c + d);
   /* code */
   return 0;
 }
-geek
